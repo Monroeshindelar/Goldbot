d655c21fd4a12d0a6d83a64ac3b8a5263568471d
Monroeshindelar <monroeshindelar@gmail.com>
2019-01-07 14:46:03 -0800

    First commit

---
+++ Goldbot/Global.cs
@@ -0,0 +1,22 @@
+﻿using CSGSI;
+using Discord.WebSocket;
+using System.Collections.Generic;
+
+namespace Goldbot {
+    internal static class Global {
+        internal static GameStateListener gsl { get; set; }
+        internal static List<SocketGuildUser> participants { get; set; }
+        internal static Dictionary<string, string> mapThumbnails { get; set; }
+        internal static Dictionary<string, bool> PickBanTracker { get; set; }
+        internal static List<SocketRole> rolesToRemove { get; set; }
+        internal static List<string> playersRemaining { get; set; }
+        internal static List<string> mapsRemaining { get; set; }
+        internal static ulong TeamAMessageID { get; set; }
+        internal static ulong TeamBMessageID { get; set; }
+        internal static ulong RemainingPlayersMessageID { get; set; }
+        internal static ulong RemainingMapsMessageID { get; set; }
+        internal static ulong MapPickMessaegID { get; set; }
+        internal static bool TeamATurn { get; set; }
+        internal static bool IsBanPhase { get; set; }
+    }
+}
---
+++ Goldbot/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Goldbot {
+    class Utilities {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities() {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string GetAlert(string key) {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+
+
+    }
+}
